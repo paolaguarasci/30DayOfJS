ecdac1af3eff8b28fd86cb94da0be7118346fd67
Paola Guarasci <paola@MacBook-Pro-di-Paola.local>
2018-06-30 11:09:14 +0200

    chage title html file

--- 05_flexGallery/script.js
+++ 05_flexGallery/script.js
@@ -7,7 +7,7 @@ function toggleActive(e) {
   console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
     this.classList.toggle('open-active');
-  }
+  } 
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
