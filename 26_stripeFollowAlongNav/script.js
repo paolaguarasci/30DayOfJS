fe0cf10415daf7736818616abe1b05eeb759385a
Paola Guarasci <paolaguarasci@gmail.com>
2018-07-20 07:47:05 +0200

    26 - stripe follow nav NON TERMINATO

---
+++ 26_stripeFollowAlongNav/script.js
@@ -0,0 +1,17 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handlerEnter() {
+  this.classList.add('trigger-enter');
+  setTimeout(() => {this.classList.add('trigger-enter-active');}, 150);
+  background.classList.add('open');
+}
+
+function handlerLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handlerEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handlerLeave));
